9b60c883b0cce4e54d3a845ad983b2f417ce16ce
Efraim Nascimento <efraimteofilocamargonascimento@outlook.com>
2021-09-30 21:13:10 -0300

    bootstrap-navbar

--- js/main.js
+++ js/main.js
@@ -7,10 +7,7 @@ function consultaCep(){
         type: "GET",
         success: function(response){
             console.log(response);
-            $("#logradouro").html(response.logradouro);
-            $("#bairro").html(response.bairro);
-            $("#localidade").html(response.localidade);
-            $("#uf").html(response.uf);
+            
             //document.getElementById('logradouro').innerHTML = response.logradouro
             //document.getElementById('bairro').innerHTML = response.bairro
             //document.getElementById('localidade').innerHTML = response.localidade
