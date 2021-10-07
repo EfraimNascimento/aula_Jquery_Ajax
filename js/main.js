f3cb7bff8f426f08aea036e6e7f08915499f6313
Efraim Nascimento <efraimteofilocamargonascimento@outlook.com>
2021-10-07 11:25:21 -0300

    progress-bar

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 function consultaCep(){
+    $(".barra-progresso").show();
     var cep = document.getElementById('cep').value;
     var url = "https://viacep.com.br/ws/"+ cep +"/json/" ;
     console.log(cep)
@@ -12,11 +13,13 @@ function consultaCep(){
             $("#localidade").html(response.localidade);
             $("#uf").html(response.uf);
             $("#titulo_cep").html("CEP " + response.cep);
-            // document.getElementById('logradouro').innerHTML = response.logradouro
-            // document.getElementById('bairro').innerHTML = response.bairro
-            // document.getElementById('localidade').innerHTML = response.localidade
-            // document.getElementById('uf').innerHTML = response.uf
-            // alert(response.logradouro)
+            $(".cep").show();
+            $(".barra-progresso").hide();
+
         }
     })
 }
+$(function(){
+    $(".cep").hide();
+    $(".barra-progresso").hide();
+});
